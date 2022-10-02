8db67cbf8ce1d914994cc206005471441c159254
vienoz <niclas.arb@outlook.com>
2022-10-02 20:06:59 +0200

    readout set to 5 minutes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from smbus import smbus2
+import csv
+import smbus
 import selftest
+import time
 from utils import Utils
 
 PORT = 1
@@ -8,14 +10,20 @@ BUS = smbus.SMBus(PORT)
 
 
 def main():
-
     if selftest.test(ADDRESS, BUS) == 0:
         print("self-test success")
 
     util = Utils(ADDRESS, BUS)
     util.initialize(1, 1, 1, 1)
 
-    print(util.readTHP())
+    while True:
+        temp, hum, pres = util.readTHP()
+        now = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        with open('measurementLog.csv', 'a') as log:
+            logWriter = csv.writer(log)
+            logWriter.writerow([temp, hum, pres, now])
+        print('successful measurement at', now)
+        time.sleep(300)
 
 
 if __name__ == '__main__':
